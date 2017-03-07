1a1e844b18db42a4c9f3ccef8be504c535c5d9b4
fabiovalse <fabiovalse@gmail.com>
2017-03-07 15:08:55 +0100

    Poi style.

--- src/Poi.vue
+++ src/Poi.vue
@@ -1,11 +1,25 @@
 <template>
-  <g 
-  :class="{target: target !== undefined && target.id == data.id}"
-  :transform="get_transform()"
-  @click="select()"
-  >
-    <circle r="8"/>
-    <text x="10" y="0" dy="0.35em">{{data.label}}</text>
+  <g
+    class="poi"
+    :transform="get_translate()"
+    @click="select()"
+    >
+    <g :transform="get_scale()">
+      <circle class="background" r="60" cy="5">
+        <title>{{data.label}}</title>
+      </circle>
+      <circle class="foreground" r="60"></circle>
+      <foreignObject x="-50" y="-35" width="100" height="100">
+        <!--<i class="icon"></i>-->
+      </foreignObject>
+      <text class="background label hidden" text-anchor="start" dy="0.35em" x="80">
+        {{data.label}}
+      </text>
+      <text class="foreground label hidden" text-anchor="start" dy="0.35em" x="80">
+        {{data.label}}
+      </text>
+    </g>
+    <title>{{data.label}}</title>
   </g>
 </template>
 
@@ -19,7 +33,8 @@ export default {
 
   methods:
     select: () -> @$store.dispatch 'select', {id: @data.id}
-    get_transform: () -> "translate(#{@data.position[0]}, #{@data.position[1]}) scale(#{if @transform? then 6/@transform.k else 1})"
+    get_translate: () -> "translate(#{@data.position[0]}, #{@data.position[1]})"
+    get_scale: () -> "scale(#{if @transform? then 1 /@transform.k else 1})"
 
 }
 </script>
@@ -28,4 +43,43 @@ export default {
 .target text {
   font-weight: bold;
 }
+
+.poi {
+  cursor: pointer;
+}
+
+.poi circle.background {
+  fill: #777;
+  stroke: #777;
+  stroke-width: 3px;
+}
+
+.poi circle.foreground {
+  fill: #FFF;
+  stroke: #DDD;
+  stroke-width: 3px;
+}
+
+.poi .icon {
+  color: #7b5b5b;
+  display: block;
+  width: 100;
+  height: 100;
+  font-size: 70;
+  text-align: center;
+}
+
+.poi .label {
+  font-size: 90px;
+}
+.poi .background.label {
+  fill: white;
+  stroke: white;
+  opacity: 0.2;
+  stroke-width: 12;
+}
+.poi .foreground.label {
+  fill: #333;
+}
+
 </style>
