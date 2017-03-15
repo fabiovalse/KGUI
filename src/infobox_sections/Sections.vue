f8f857005d834c8477807b99d932286090a986cf
nitaku <matteo.abrate@gmail.com>
2017-03-15 15:15:32 +0100

    Commit of previosuly omitted file.

---
+++ src/infobox_sections/Sections.vue
@@ -0,0 +1,26 @@
+<script lang="coffee">
+import config from '../config.coffee'
+import TextSection from './TextSection.vue'
+import HeaderSection from './HeaderSection.vue'
+import ImageSection from './ImageSection.vue'
+import ListSection from './ListSection.vue'
+import SeparatorSection from './SeparatorSection.vue'
+
+export default {
+  render: (createElement) ->
+    children = @template.map (section) => createElement section.t+'section', {props:{data:@target, config:section}}
+    return createElement 'div', {class: {sections: true}}, children
+  computed:
+    target: () -> @$store.state.target
+    template: () -> if config.templates? and @target.template? then config.templates[@target.template] else []
+  components:
+    textsection: TextSection
+    headersection: HeaderSection
+    imagesection: ImageSection
+    listsection: ListSection
+    separatorsection: SeparatorSection
+}
+</script>
+
+<style scoped>
+</style>
