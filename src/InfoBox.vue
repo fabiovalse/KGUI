e63120a46960ae99e5c945ad37c5e9889030f66a
nitaku <matteo.abrate@gmail.com>
2017-03-15 14:49:00 +0100

    Sections component separated from infobox.

--- src/InfoBox.vue
+++ src/InfoBox.vue
@@ -1,15 +1,14 @@
+<template>
+  <div class="infobox">
+    <div class="expand"><span v-if="mobile_open"><i class="icon-slide-down"></i></span><span v-else><i class="icon-slide-up"></i></span></div>
+    <sections v-if="target !== undefined"></sections>
+  </div>
+</template>
+
 <script lang="coffee">
-import config from './config.coffee'
-import TextSection from './infobox_sections/TextSection.vue'
-import HeaderSection from './infobox_sections/HeaderSection.vue'
-import ImageSection from './infobox_sections/ImageSection.vue'
-import ListSection from './infobox_sections/ListSection.vue'
-import SeparatorSection from './infobox_sections/SeparatorSection.vue'
+import Sections from './infobox_sections/Sections.vue'
 
 export default {
-  render: (createElement) ->
-    children = @template.map (section) => createElement section.t+'section', {props:{data:@target, config:section}}
-    return createElement 'div', {class: {infobox: true, mobile_open: @mobile_open}, on: {click: @click}}, children
   props:
     searchdirectionsbox_enabled:
       type: Boolean
@@ -18,17 +17,11 @@ export default {
   computed:
     mode: () -> @$store.state.mode
     target: () -> @$store.state.target
-    template: () -> if config.templates? and @target.template? then config.templates[@target.template] else []
-  methods:
+  methods: # FIXME some methods are unused
     click_directions: () -> @$store.dispatch 'request_directions', {def: true}
     click_node: (d) -> @$store.dispatch 'select', {id: d.id}
-    click: () -> @$emit 'mobile_open'
   components:
-    textsection: TextSection
-    headersection: HeaderSection
-    imagesection: ImageSection
-    listsection: ListSection
-    separatorsection: SeparatorSection
+    sections: Sections
 }
 </script>
 
@@ -36,52 +29,7 @@ export default {
 .infobox {
   box-shadow: 0 0 20px rgba(0,0,0,0.3);
 }
-.profile {
-  position: relative;
-  display: flex;
-  padding: 10px;
-  font-size: 13px;
-  color: #fff;
-  background: #b44646;
-  min-height: 68px;
-}
-.profile .label {
-  font-size: 21px;
-}
-.node {
-  cursor: pointer;
-}
-.mobile_open {
-  top: 0%;
-  margin-top: 0px;
-}
-button {
-  position: absolute;
-  top: -35%;
-  right: 20px;
 
-  display: flex;
-  justify-content: center;
-  width: 60px;
-  height: 60px;
-  font-size: 20px;
-
-  border-radius: 50%;
-  border: none;
-
-  cursor: pointer;
-  background: #FFF;
-  color: #00B3FD;
-  box-shadow: 0 1px 6px 0 rgba(0,0,0,.3);
-}
-button i {
-  height: 20px;
-}
-
-.target_info, .directions_info {
-  display: flex;
-  flex-direction: column;
-}
 .expand {
   display: none;
   text-align: center;
@@ -94,11 +42,6 @@ button i {
   }
   .expand {
     display: inline;
-  }
-  button {
-    right: 10px;
-    width: 48px;
-    height: 48px;
   }
 }
 
