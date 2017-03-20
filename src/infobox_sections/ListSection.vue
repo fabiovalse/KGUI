01f9bd2ed73eb5cdf9902cdf71a085193a8e333b
fabiovalse <fabiovalse@gmail.com>
2017-03-20 15:55:27 +0100

    Handling icons.

--- src/infobox_sections/ListSection.vue
+++ src/infobox_sections/ListSection.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="list_section">
     <table v-if="items.length > 0">
-      <tr v-for="item in items"><td class="label" v-html="item.label"></td><td class="value" v-html="item.value"></td></tr>
+      <tr v-for="item in items">
+        <td>
+          <i :class="'icon icon-' + item.icon"></i>
+        </td>
+        <td class="value" v-html="item.value"></td>
+      </tr>
     </table>
   </div>
 </template>
@@ -17,14 +22,17 @@ export default {
     config:
       type: Object
       required: true
+  
   computed:
     items: () ->
       list = @config.items.map (item) => {
-          label: kgl.parse(item.label, @data)
-          value: kgl.parse(item.value, @data)
-        }
+        label: kgl.parse(item.label, @data)
+        value: kgl.parse(item.value, @data)
+        icon: item.icon
+      }
       # keep defined pairs only
       return list.filter (d) -> d.label? and d.value?
+
 }
 </script>
 
@@ -39,6 +47,11 @@ table {
 
 td:not(:last-child) {
   padding-right: 8px;
+}
+
+.icon {
+  font-size: 18px;
+  color: #B44646;
 }
 
 </style>
