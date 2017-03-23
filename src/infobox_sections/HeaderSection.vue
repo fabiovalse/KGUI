8580ef63ea5f307bf06b984e481d7c4bb149b521
fabiovalse <fabiovalse@gmail.com>
2017-03-23 10:13:19 +0100

    Added icon links on top-right.

--- src/infobox_sections/HeaderSection.vue
+++ src/infobox_sections/HeaderSection.vue
@@ -2,6 +2,11 @@
   <div class="header_section" @click="click">
     <div class="title" v-if="config.title !== undefined" v-html="kgl_parse(config.title, data)"></div>
     <div class="subtitle" v-if="config.subtitle !== undefined" v-html="kgl_parse(config.subtitle, data)"></div>
+    <div class="hlinks">
+      <a v-for="img in config.hlinks" :href="img.url" target="blank">
+        <img :src="img.icon" :title="img.label">
+      </a>
+    </div>
   </div>
 </template>
 
@@ -25,6 +30,7 @@ export default {
 
 <style scoped>
 .header_section {
+  position: relative;
   padding: 30px 25px 20px 25px;
   font-family: sans-serif;
   background: #B44646;
@@ -39,4 +45,13 @@ export default {
   font-size: 15px;
 }
 
+.hlinks {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+.hlinks img {
+  width: 20px;
+  padding: 5px 3px 5px 3px;
+}
 </style>
