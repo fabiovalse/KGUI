4adf4390685ea93a5064b0c57228d3173eab4e79
kleem <fabiovalse@gmail>
2017-03-30 16:16:07 +0200

    Added action sections.

---
+++ src/infobox_sections/ActionSection.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="action_section">
+    <div v-for="action in actions" class="action">
+      <a :href="'#/'+action.id+'/fullmap'">
+        <div><i :class="'icon-'+action.icon"></i></div>
+        <div>{{action.label}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script lang="coffee">
+import kgl from './kgl.coffee'
+
+export default {
+  props:
+    data:
+      type: Object
+      required: true
+    config:
+      type: Object
+      required: true
+
+  computed:
+    actions: () -> 
+      @config.actions.map (action) => {
+        id: kgl.parse(action.id, @data)
+        icon: action.icon
+        label: kgl.parse(action.label, @data)
+      }
+
+}
+</script>
+
+<style scoped>
+.action_section {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 83px;
+}
+
+.action {
+  width: 100px;
+  text-align: center;
+  padding: 5px;
+}
+</style>
