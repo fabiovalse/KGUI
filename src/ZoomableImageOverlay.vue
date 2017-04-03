64f26c2d17f16aee57f88917192ed5a9d35703a4
kleem <fabiovalse@gmail>
2017-04-03 16:37:17 +0200

    Click to zoom on annotations.

--- src/ZoomableImageOverlay.vue
+++ src/ZoomableImageOverlay.vue
@@ -1,6 +1,6 @@
 <template>
   <g>
-    <g v-for="obj in data" :class="{selected: selected.id === obj.id}" @click="select(obj.id)">
+    <g v-for="obj in data" :class="get_classes(obj)" @click="select(obj.id)">
       <rect v-if="obj.data.selector === 'rect'" :x="obj.data.x1" :y="obj.data.y1" :width="obj.data.x2-obj.data.x1" :height="obj.data.y2-obj.data.y1"></rect>
       <circle v-if="obj.data.selector === 'circle'" :cx="obj.data.cx" :cy="obj.data.cy" :r="obj.data.rx-obj.data.cx"></circle>
       <path v-if="obj.data.selector === 'path'" :d="to_path_description(obj.data.points)"></path>
@@ -16,12 +16,26 @@ export default {
       required: true
     store:
       type: Object
+    viewer:
+      type: Object
 
   computed:
     selected: () -> @store.state.target
 
   methods:
-    select: (id) -> @store.dispatch 'select', {id: id}
+    select: (id) -> 
+      @store.dispatch 'select', {id: id}
+
+      if @selected.id is id
+        bbox = @$el.querySelector(".obj#{id}").getBBox()
+        @viewer.viewport.fitBounds new OpenSeadragon.Rect(bbox.x, bbox.y, bbox.width, bbox.height)
+    get_classes: (obj) ->
+      classes = {
+        selected: @selected? and @selected.id is obj.id
+      }
+      classes["obj#{obj.id}"] = true
+      return classes
+
     to_path_description: (points) ->
       points = points.concat([points[0]])
       return (points.map (p,i) -> return if i is 0 then "M#{points[i].x} #{points[i].y}" else "L #{points[i].x} #{points[i].y} ").join('')
@@ -31,15 +45,17 @@ export default {
 
 <style scoped>
 rect, circle, path {
-  fill: rgba(70, 130, 180, 0.5);
-  stroke: steelblue;
-  stroke-width: 3px;
+  fill: #ffd26d;
   vector-effect: non-scaling-stroke;
   cursor: pointer;
 }
 
+rect:hover, circle:hover, path:hover {
+  stroke: #ff9e72;
+  stroke-width: 3px;
+}
+
 .selected rect, .selected circle, .selected path {
-  fill: rgba(180, 70, 70, 0.5);
-  stroke: #B44646;
+  fill: #ff9e72;
 }
 </style>
