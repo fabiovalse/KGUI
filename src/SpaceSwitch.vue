2c017a96e6b9aa9bbc0c8ad2be3123444658e86e
kleem <fabiovalse@gmail>
2017-04-07 13:42:21 +0200

    Basic space switch (version with n squares)

--- src/SpaceSwitch.vue
+++ src/SpaceSwitch.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="spaceswitch">
-    <div v-for="s in spaces">
+    <!--<div v-for="s in spaces">
       <input type="radio" name="spaces" :checked="s.id === space.id" @click="change_space(s.id)"> {{s.id}}
+    </div>-->
+    <div class="space" :class="{selected: s.id === space.id}" :style="get_style(s.thumbnail)" v-for="s in spaces" @click="change_space(s.id)">
+      <div>{{s.label}}</div> 
     </div>
   </div>
 </template>
@@ -14,20 +17,40 @@ export default {
   
   methods:
     change_space: (id) -> @$store.dispatch 'change_space', id
+    get_style: (url) -> "background-image: url(#{url}); background-size: 40px;"
 }
 </script>
 
 <style scoped>
 .spaceswitch {
-  cursor: pointer;
-  width: 300px;
-  height: 80px;
-  background: white;
-  border: 1px solid #BBB;
-
+  display: flex;
   position: absolute;
-  bottom: 5px;
-  right: 5px;
+  bottom: 20px;
+  right: 20px;
   z-index: 2;
+}
+
+.space {
+  width: 40px;
+  height: 40px;
+  background: #FFF;
+  margin-left: 10px;
+  border-radius: 3px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.2), 0 -1px 0px rgba(0,0,0,0.02);
+  cursor: pointer; 
+  opacity: 0.5;
+}
+.space:hover {
+  opacity: 1;
+}
+.space div {
+  line-height: 60px;
+  font-size: 12px;
+  text-align: center;
+  color: #303030;
+  font-weight: bold;
+}
+.selected {
+  opacity: 1;
 }
 </style>
