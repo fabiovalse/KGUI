5f53d5956c3fd42d0dd81df19ed7bd0ae72f1b0e
fabiovalse <fabiovalse@gmail.com>
2017-04-19 11:45:07 +0200

    Improved style and layout

--- src/infobox_sections/BilingualTextSection.vue
+++ src/infobox_sections/BilingualTextSection.vue
@@ -1,7 +1,14 @@
 <template>
   <div v-if="text !== undefined" class="bilingualtext_section">
-    <div class="large">{{text[0]}}</div>
-    <div>{{text[1]}}</div>
+    <div>
+      <div class="large">{{text[0]}}</div>
+      <div class="language">{{text_description[0]}}</div>  
+    </div>
+    <br>
+    <div>
+      <div>{{text[1]}}</div>
+      <div class="language">{{text_description[1]}}</div>
+    </div>
   </div>
 </template>
 
@@ -19,6 +26,7 @@ export default {
 
   computed:
     text: () -> kgl.parse(@config.text, @data)
+    text_description: () -> kgl.parse(@config.text_description, @data)
 
 }
 </script>
@@ -26,16 +34,16 @@ export default {
 <style scoped>
 .bilingualtext_section {
   display: flex;
-  align-items: center;
-  justify-content: space-between;
+  flex-direction: column;
   padding: 24px;
-}
-
-.bilingualtext_section > * {
-  width: 45%;
 }
 
 .large {
   font-size: 30px;
+}
+
+.language {
+  font-size: 12px;
+  color: rgba(0,0,0,0.54);
 }
 </style>
