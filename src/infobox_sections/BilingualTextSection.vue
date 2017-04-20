d92edf53ba840765e3b6f7386c8e8e169972100c
fabiovalse <fabiovalse@gmail.com>
2017-04-20 10:16:58 +0200

    Handling notes.

--- src/infobox_sections/BilingualTextSection.vue
+++ src/infobox_sections/BilingualTextSection.vue
@@ -1,13 +1,19 @@
 <template>
   <div v-if="text !== undefined" class="bilingualtext_section">
     <div>
+      <div class="language">{{text_description[0]}}</div>
       <div class="large">{{text[0]}}</div>
-      <div class="language">{{text_description[0]}}</div>  
     </div>
     <br>
     <div>
-      <div>{{text[1]}}</div>
-      <div class="language">{{text_description[1]}}</div>
+      <div class="language" v-html="text_description[1]"></div>
+      <div v-html="text[1]"></div>
+      
+      <div class="notes">
+        <div v-for="(note,i) in notes" class="note">
+          <sup>{{note_ids[i]}}</sup> <span>{{note}}</span>
+        </div>  
+      </div>
     </div>
   </div>
 </template>
@@ -25,8 +31,10 @@ export default {
       required: true
 
   computed:
-    text: () -> kgl.parse(@config.text, @data)
-    text_description: () -> kgl.parse(@config.text_description, @data)
+    text: () -> kgl.parse(@config.text, @data, true)
+    text_description: () -> kgl.parse(@config.text_description, @data, true)
+    notes: () -> kgl.parse(@config.notes, @data)
+    note_ids: () -> kgl.parse(@config.note_ids, @data)
 
 }
 </script>
@@ -42,8 +50,16 @@ export default {
   font-size: 30px;
 }
 
-.language {
+.language, .notes {
   font-size: 12px;
   color: rgba(0,0,0,0.54);
+}
+.notes {
+  margin-top: 15px;
+  padding-top: 15px;
+  border-top: 1px solid #BBB;
+}
+.note {
+  padding-bottom: 10px;
 }
 </style>
