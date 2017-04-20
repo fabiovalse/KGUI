ea84b1f2f042a85d2e6839fceb23fa610b575545
fabiovalse <fabiovalse@gmail.com>
2017-04-20 14:21:28 +0200

    Added notes.

--- src/BilingualTextView.vue
+++ src/BilingualTextView.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="bilingualtextview">
     <table cellspacing="0">
-      <tr v-for="row in parse()">
-        <td><annotatedtext :config="{text: row[0]}" :data="{}"></annotatedtext></td>
-        <td><annotatedtext :config="{text: row[1]}" :data="{}"></annotatedtext></td>
+      <tr v-for="(row,i) in bilingualtext">
+        <th v-if="i === 0">{{bilingualtext[0][0]}}</th>
+        <th v-if="i === 0">{{bilingualtext[0][1]}}</th>
+
+        <td v-if="i > 0"><annotatedtext :config="{text: row[0]}" :data="{}"></annotatedtext></td>
+        <td v-if="i > 0"><annotatedtext :config="{text: row[1]}" :data="{}"></annotatedtext></td>
       </tr>
     </table>
+
+    <div class="notes">
+      <div v-for="(note,i) in notes" class="note">
+        <sup>{{note_ids[i]}}</sup> <span>{{note}}</span>
+      </div>  
+    </div>
   </div>
 </template>
 
@@ -15,10 +24,9 @@ import AnnotatedText from './AnnotatedText.vue'
 export default {
 
   computed:
-    space: () -> @$store.state.space
-
-  methods:
-    parse: () -> JSON.parse(@space.bilingualtext)
+    bilingualtext: () -> JSON.parse(@$store.state.space.bilingualtext)
+    notes: () -> @$store.state.space.notes
+    note_ids: () -> @$store.state.space.note_ids
 
   components:
     annotatedtext: AnnotatedText
@@ -37,16 +45,23 @@ export default {
   table {
     padding: 20px;
   }
-
   td {
     padding: 10px;
     text-align: justify;
     border-bottom: 1px solid antiquewhite;
   }
-
   tr td:first-child {
     font-size: 20px;
     letter-spacing: 2px;
+  }
+
+  .notes {
+    padding: 20px;
+    font-size: 12px;
+    color: rgba(0,0,0,0.54);
+  }
+  .note {
+    padding-bottom: 10px;
   }
 
   @media (max-width: 480px) {
