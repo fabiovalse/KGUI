7109fc507c329d6923187f579b2bb01d199b72a1
fabiovalse <fabiovalse@gmail.com>
2017-04-27 14:28:51 +0200

    Added fake image for text like graphemes.

--- src/infobox_sections/ImageSection.vue
+++ src/infobox_sections/ImageSection.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="image_section" :style="{'background-image': src ? 'url('+src+')' : 'none'}" >
+    <div v-if="label !== undefined" class="fake_image" :style="{'font-family': font_family}">
+      <div class="text">{{label}}</div>
+    </div>
   </div>
 </template>
 
@@ -16,6 +19,8 @@ export default {
       required: true
   computed:
     src: () -> kgl.parse(@config.href, @data, false)
+    label: () -> kgl.parse(@config.label, @data, false)
+    font_family: () -> @config.font
 }
 </script>
 
@@ -25,6 +30,20 @@ export default {
   height: 214px;
   background-size: cover;
   background-color: #EEE;
+}
+
+.fake_image {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  font-size: 200px;
+  background: url(../../data/img/grapheme_background.png);
+}
+.text {
+  line-height: 350px;
+  color: #353535;
+  text-shadow: 0px 0px 30px rgba(150, 150, 150, 0.9);
+  pointer-events: none;
 }
 
 @media (max-width: 480px){
