e8ad0f8c56cf51c1db2e3dcec54d5d520cfc78ce
fabiovalse <fabiovalse@gmail.com>
2017-05-03 10:25:37 +0200

    Fixed domain computation.

--- src/infobox_sections/TimeSection.vue
+++ src/infobox_sections/TimeSection.vue
@@ -2,17 +2,17 @@
   <svg v-if="start_date !== undefined && end_date !== undefined" class="time_section">
     <g :transform="get_translate()">
       <rect class="bar" :x="scale_value(start_date)" y="35" :width="scale_value(end_date)-scale_value(start_date)"></rect>
-      <text :x="scale_value(start_date)" y="30">{{start_date}}</text>
-      <text :x="scale_value(end_date)" y="30">{{end_date}}</text>
+      <text :x="scale_value(start_date)-5" y="30">{{start_date}}</text>
+      <text :x="scale_value(end_date)+5" y="30">{{end_date}}</text>
       <text class="years" :x="scale_value(start_date)+(scale_value(end_date)-scale_value(start_date))/2" y="46">{{end_date-start_date}}</text>
     </g>
     
     <g class="reference" v-if="data.id !== config.ref.id">
       <rect class="bar" :x="scale_value(ref.start_date)" y="57" :width="scale_value(ref.end_date)-scale_value(ref.start_date)"></rect>
-      <text :x="scale_value(ref.start_date)" y="84">{{ref.start_date}}</text>
-      <text :x="scale_value(ref.end_date)" y="84">{{ref.end_date}}</text>
+      <text :x="scale_value(ref.start_date)-5" y="84">{{ref.start_date}}</text>
+      <text :x="scale_value(ref.end_date)+5" y="84">{{ref.end_date}}</text>
       <text :x="scale_value(ref.start_date)+(scale_value(ref.end_date)-scale_value(ref.start_date))/2" y="84">{{ref.label}}</text>
-      <text class="years" :x="scale_value(ref.start_date)+(scale_value(ref.end_date)-scale_value(ref.start_date))/2" y="67">{{ref.end_date-ref.start_date}}</text>
+      <text class="years" :x="scale_value(ref.start_date)+(scale_value(ref.end_date)-scale_value(ref.start_date))/2" y="68">{{ref.end_date-ref.start_date}}</text>
     </g>
   </svg>
 </template>
@@ -30,14 +30,12 @@ export default {
       type: Object
       required: true
 
-  data: () ->
-    scale: d3.scaleLinear().domain(@config.domain).range(@config.range)
-
   computed:
     start_date: () -> kgl.parse(@config.start_date, @data)
     end_date: () -> kgl.parse(@config.end_date, @data)
     ref: () -> @config.ref
     height: () -> if @height? then @height else undefined
+    scale: () -> d3.scaleLinear().domain([Math.min(@config.ref.start_date, @start_date)-50, Math.max(@config.ref.end_date, @end_date)+50]).range(@config.range)
 
   methods:
     scale_value: (value) -> @scale(value)
