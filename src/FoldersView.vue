b1c4902f3051a7d9fd4810f1d63047cac51759f3
fabiovalse <fabiovalse@gmail.com>
2017-05-03 10:27:03 +0200

    Improved layout and style.

--- src/FoldersView.vue
+++ src/FoldersView.vue
@@ -3,9 +3,15 @@
     <div class="margin"></div>
     <div class="folders">
       <div v-for="folder in folders"
-           class="folder_container">
-        <div class="folder" @click="open(folder)">
-          <div class="icon"><i class="icon-folder"></i></div>
+           class="folder_container"
+           :style="{width: folder.width !== undefined ? 25*folder.width + '%' : '25%'}">
+        <div class="folder" 
+             :class="{vfs_img: folder.vfs_img !== undefined}"
+             @click="click(folder)"
+             :style="{height: folder.height !== undefined ? 124*folder.width + 'px' : '124px', background: 'url('+folder.vfs_img+') #9c3535'}">
+          <div v-if="folder.icon !== undefined" class="icon">
+            <i :class="'icon-' + folder.icon"></i>
+          </div>
           <div class="label">{{folder.label}}</div>
         </div>
       </div>
@@ -16,9 +22,26 @@
 <script lang="coffee">
 export default {
   computed:
-    folders: () -> @$store.state.space.subspaces
+    folders: () ->
+      @$store.state.space.subspaces.sort (a,b) -> 
+        if not a.width?
+          a.width = 1
+        if not b.width?
+          b.width = 1
+
+        if b.width is a.width
+          if b.view?
+            return 1
+          else
+            return -1
+        else
+          return b.width - a.width
   methods:
-    open: (item) -> @$store.dispatch 'change_space', item.id
+    click: (item) -> 
+      if item.view?
+        @$store.dispatch 'change_space', item.id
+      else
+        @$store.dispatch 'select', {id: item.id}
 }
 </script>
 
@@ -26,7 +49,7 @@ export default {
 .foldersview {
   height: 100%;
   overflow-y: scroll;
-  background: #7b5b5b;
+  background: rgb(230,224,207);
   display: flex;
   padding-top: 60px; /* FIXME this is because of the breadcrumb, but we need a more intelligent way to do this */
   padding-left: 420px; /* 12px more, needed for folders margin */
@@ -37,33 +60,44 @@ export default {
   height: 100%;
 }
 .folder_container {
-  width: 25%;
+  /*width: 25%;*/
   display: inline-block;
 }
 .folder {
-  height: 124px;
+  position: relative;
   margin: 12px;
   margin-left: 0;
-  box-shadow: 4px 4px 6px rgba(0,0,0,0.4);
-  border-radius: 3px;
-  background: rgb(253,233,153);
-  position: relative;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.2), 0 -1px 0px rgba(0,0,0,0.02);
   cursor: pointer;
+  background-position-x: center !important;
+  background-position-y: top !important;
+  background-size: 100% !important;
+  background-repeat: no-repeat !important;
 }
+
+.icon {
+  display: table;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  font-size: 65px;
+  color: #FAFAFA;
+}
+.icon i {
+  display: table-cell;
+  vertical-align: middle;
+}
+
 .label {
   position: absolute;
-  bottom: 12px;
-  left: 40px;
+  bottom: 5px;
+  left: 10px;
   pointer-events: none;
   font-size: 13px;
+  color: #FAFAFA;
+  text-shadow: 0px 0px 2px rgba(0, 0, 0, 1);
 }
-.icon {
-  position: absolute;
-  bottom: 5px;
-  left: 12px;
-  font-size: 20px;
-  color: rgb(230, 194, 60);
-}
+
 @media (max-width: 480px) {
   .foldersview {
     padding-top: 60px; /* 12px more, needed for folders margin */
