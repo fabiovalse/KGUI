65d88d98e30f77c18411f51c64643a15b9e8e543
fabiovalse <fabiovalse@gmail.com>
2017-05-10 16:45:26 +0200

    Changed folder interaction. Changed layout and style.

--- src/FoldersView.vue
+++ src/FoldersView.vue
@@ -2,37 +2,34 @@
   <div class="foldersview">
     <div class="margin"></div>
     <div class="folders">
-      <div v-for="folder in folders"
-           class="folder_container"
-           :style="{width: folder.width !== undefined ? 25*folder.width + '%' : '25%', 'max-width': folder.width !== undefined ? folder.width*204+'px' : '204px'}">
         
-        <div class="folder"
-             @click="select(folder)"
-             :class="{vfs_img: folder.vfs_img !== undefined}"
-             :style="{height: folder.height !== undefined ? 204*folder.width + 'px' : '204px'}">
+      <div class="folder" v-for="folder in folders"
+            @click="open(folder)"
+            :class="{vfs_img: folder.vfs_img !== undefined}"
+            :style="{
+              width: folder.width === 2 ? '384px' : '250px',
+              height: folder.height === 2 ? '361px' : '250px'}">
 
-          <div v-if="folder.icon !== undefined" class="icon">
-            <i :class="'icon-' + folder.icon"></i>
-          </div>
-
-          <div v-if="folder.icon == undefined"
-               class="img"
-               :style="{background: 'url('+folder.vfs_img+') #B44646', 'background-position-x': 'center !important', 'background-size': 'cover'}"></div>
-          
-          <div class="title">
-            <div class="main">{{folder.label.toUpperCase()}}</div>
-            <div class="sub">{{folder.sub_title}}</div>
-          </div>
-
-          
-          <button v-if="folder.view !== undefined" class="button" @click="open(folder)">
-            <div>
-              <i class="icon-zoom_in"></i>
-            </div>
-          </button>
+        <div v-if="folder.icon !== undefined" class="icon">
+          <i :class="'icon-' + folder.icon"></i>
+        </div>
+        <div v-if="folder.icon == undefined"
+              class="img"
+              :style="{
+                background: 'url('+folder.vfs_img+') #DDD',
+                'background-position-x': 'center !important',
+                'background-size': 'cover'}"></div>
+        
+        <div class="title">
+          <div class="main">{{folder.label.toUpperCase()}}</div>
+          <div class="sub">{{folder.sub_title}}</div>
         </div>
 
+        <div v-if="folder.view !== undefined" class="type">
+          <i :class="folder.view === 'foldersview' ? 'icon-folder' : 'icon-zoom_in'"></i>
+        </div>
       </div>
+
     </div>
   </div>
 </template>
@@ -55,11 +52,12 @@ export default {
         else
           return b.width - a.width
   methods:
-    select: (item) -> 
-      @$store.dispatch 'select', {id: item.id}
+    open: (item) -> 
+      if item.view?
+        @$store.dispatch 'change_space', item.id
 
-    open: (item) ->
-      @$store.dispatch 'change_space', item.id
+      if item.template?
+        @$store.dispatch 'select', {id: item.id}
 }
 </script>
 
@@ -67,7 +65,7 @@ export default {
 .foldersview {
   height: 100%;
   overflow-y: scroll;
-  background: #b6b6b6;
+  background: #f5f5f5;
   padding-top: 60px; /* FIXME this is because of the breadcrumb, but we need a more intelligent way to do this */
   padding-left: 430px; /* 12px more, needed for folders margin */
   box-sizing: border-box;
@@ -76,14 +74,14 @@ export default {
   display: flex;
   flex-wrap: wrap;
   align-items: flex-end;
+  margin-left: -8px;
 }
 .folder {
   display: flex;
   flex-direction: column;
   position: relative;
-  margin: 3px 6px 3px 0px;
-  margin-left: 0;
-  background: #B44646;
+  margin: 8px;
+  background: #DDD;
   box-shadow: 0 2px 4px rgba(0,0,0,0.2), 0 -1px 0px rgba(0,0,0,0.02);
   cursor: pointer;
   border-radius: 2px;
@@ -92,7 +90,7 @@ export default {
 .icon {
   flex-grow: 1;
   width: 100%;
-  padding-top: 60px;
+  padding-top: 35px;
   text-align: center;
   font-size: 52px;
   background: #DDD;
@@ -102,35 +100,38 @@ export default {
 .img {
   flex-grow: 1;
   width: 100%;
+  border-top-right-radius: 2px;
+  border-top-left-radius: 2px;
 }
 
 .title {
-  height: 40px;
+  height: 75px;
   pointer-events: none;
   background: #FFF;
+  padding: 24px 48px 24px 16px;
+  border-bottom-right-radius: 2px;
+  border-bottom-left-radius: 2px;
 }
 .title .main {
   font-size: 12px;
   font-weight: bold;
-  padding: 5px 0px 0px 10px;
   color: #212121;
 }
 .title .sub {
-  font-size: 13px;
-  padding: 0px 0px 0px 10px;
+  font-size: 15px;
   color: rgba(0,0,0,0.54);
+  padding-top: 3px;
 }
 
-.button {
+.type {
   position: absolute;
-  right: 7px;
+  right: 11px;
   bottom: 45px;
   border: none;
   border-radius: 50%;
   background: transparent;
-  font-size: 17px;
-  text-shadow: 0px 0px 4px rgba(0,0,0,0.5);
-  color: #FFF;
+  font-size: 18px;
+  color: #757575;
   line-height: 0px;
   padding: 5px;
   cursor: pointer;
