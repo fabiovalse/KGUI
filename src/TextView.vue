7aefc59e1cf2b2d104996303a64ca8afc7c03360
nitaku <matteo.abrate@gmail.com>
2017-06-15 16:02:11 +0200

    Images from text views

--- src/TextView.vue
+++ src/TextView.vue
@@ -2,13 +2,18 @@
   <div class="textview">
     <spaceheader></spaceheader>
 
-    <div class="page" v-for="page in pages">
+    <div class="page" v-for="(page, i) in pages">
+      <router-link :to="{name: 'goto_space', params: {space: get_folio_id(i)}}">
+        <div class="preview" :style="{'background': get_folio_img(i)}"></div>
+        <div class="explore"><i class="icon-zoom_in"></i></div>
+      </router-link>
       <div class="text" v-html="page"></div>
     </div>
   </div>
 </template>
 
 <script lang="coffee">
+import config from './config.coffee'
 import kgl from './infobox_sections/kgl.coffee'
 import SpaceHeader from './SpaceHeader.vue'
 
@@ -21,6 +26,10 @@ export default {
         d = kgl.parse(d, @$store.state.selection.space)
         return d
       return ps
+    folii: () -> @$store.state.selection.space.subspaces.sort (a,b) -> a.order - b.order
+  methods:
+    get_folio_id: (i) -> if @folii[i]? then @folii[i].id else undefined
+    get_folio_img: (i) -> if @folii[i]? then 'url('+config.main_uri+'/images/depictions/'+@folii[i].id+'.jpg) black' else 'black'
   components:
     spaceheader: SpaceHeader
 }
@@ -39,5 +48,33 @@ export default {
   border-bottom: 1px solid #DDD;
   padding-bottom: 16px;
   margin-bottom: 16px;
+}
+.page {
+  display: flex;
+  flex-direction: row;
+}
+.preview {
+  width: 300px;
+  height: 400px;
+  margin-right: 20px;
+  background-position-x: center !important;
+  background-size: contain !important;
+  background-repeat: no-repeat !important;
+}
+.explore {
+  --width: 50px;
+  position: relative;
+  bottom: calc(var(--width) / 2);
+  left: calc(100% - var(--width) * 2);
+  width: var(--width);
+  height: var(--width);
+  color: white;
+  background: var(--main-color);
+  border-radius: calc(var(--width) / 2);
+  text-align: center;
+  line-height: 55px;
+  font-size: 18px;
+  box-shadow: 0 0 4px rgba(0,0,0,.14), 0 4px 8px rgba(0,0,0,.28);
+  cursor: pointer;
 }
 </style>
