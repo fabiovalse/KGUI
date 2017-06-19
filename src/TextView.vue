be37e616c1ed7a7efc5096cda8426a4ea7e227e8
nitaku <matteo.abrate@gmail.com>
2017-06-19 11:35:30 +0200

    Addendum to previous commit

--- src/TextView.vue
+++ src/TextView.vue
@@ -71,10 +71,13 @@ export default {
   color: white;
   background: var(--main-color);
   border-radius: calc(var(--width) / 2);
-  text-align: center;
-  line-height: 55px;
-  font-size: 18px;
   box-shadow: 0 0 4px rgba(0,0,0,.14), 0 4px 8px rgba(0,0,0,.28);
   cursor: pointer;
+}
+.explore i {
+  display: block;
+  padding-top: 16px;
+  text-align: center;
+  font-size: 18px;
 }
 </style>
