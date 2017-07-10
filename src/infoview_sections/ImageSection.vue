e6000868641747d43f72d366b1780267bdc65898
fabiovalse <fabiovalse@gmail.com>
2017-07-10 16:51:21 +0200

    Added image section.

---
+++ src/infoview_sections/ImageSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="imagesection">
+    <img :src="url" />
+  </div>
+</template>
+
+<script lang="coffee">
+import kgl from '../infobox_sections/kgl.coffee'
+
+export default {
+  props:
+    data:
+      type: Object
+      required: true
+    config:
+      type: Object
+      required: true
+
+  computed:
+    url: () -> kgl.parse(@config.href, @data, false)
+
+}
+</script>
+
+<style scoped>
+.imagesection {
+  margin-top: 20px;
+}
+.imagesection img {
+  width: 100%;
+  border-radius: 2px;
+}
+</style>
