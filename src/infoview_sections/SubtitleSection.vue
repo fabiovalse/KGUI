533fb5801d2a668f410e4410953dc51dcf505527
fabiovalse <fabiovalse@gmail.com>
2017-07-10 12:24:57 +0200

    Added new sections.

---
+++ src/infoview_sections/SubtitleSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="subtitlesection" v-html="text"></div>
+</template>
+
+<script lang="coffee">
+import kgl from '../infobox_sections/kgl.coffee'
+
+export default {
+  props:
+    data:
+      type: Object
+      required: true
+    config:
+      type: Object
+      required: true
+
+  computed:
+    text: () -> kgl.parse(@config.text, @data)
+
+}
+</script>
+
+<style scoped>
+.subtitlesection {
+  margin-top: 20px;
+  font-size: 20px;
+  font-weight: 300;
+  color: #333;
+}
+</style>
