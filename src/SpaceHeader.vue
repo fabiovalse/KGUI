0e7724448404b8261c618e7c7a27df62e945c3b7
fabiovalse <fabiovalse@gmail.com>
2017-07-10 12:22:59 +0200

    Section mechanism restructuring.

--- src/SpaceHeader.vue
+++ src/SpaceHeader.vue
@@ -1,36 +1,12 @@
 <template>
   <header v-if="!hidden" :class="{overlay: overlay}">
-    <div v-if="space.label !== undefined" class="label" v-html="kgl(space.label)"></div>
-
-    <div class="mailing" v-if="space.view === 'manuscriptview'">
-      <div class="sender">
-        <div class="img"><i class="icon-user-circle"></i></div>
-        <div class="info">
-          <div class="name">{{space.sender}}</div>
-          <div class="place">{{space.sender_place}}</div>
-          <div class="date">{{space.date}}</div>
-        </div>
-      </div>
-      <div class="mail">
-        <div class="arrow"><i class="icon-arrow-up"></i></div>
-        <div><i class="icon-email"></i></div>
-      </div>
-      <div class="recipient">
-        <div class="img"><i class="icon-user-circle"></i></div>
-        <div class="info">
-          <div class="name">{{space.recipient}}</div>
-          <div class="place">{{space.recipient_place}}</div>
-        </div>
-      </div>
-    </div>
-    <div v-else-if="space.subtitle !== undefined" class="subtitle" v-html="kgl(space.subtitle)"></div>
-
-    <div v-if="space.description !== undefined" class="description" v-html="kgl(space.description)"></div>
+    <sections :template_type="'header'"></sections>
   </header>
 </template>
 
 <script lang="coffee">
 import kgl from './infobox_sections/kgl.coffee'
+import Sections from './infoview_sections/Sections.vue'
 
 export default {
   props:
@@ -42,6 +18,9 @@ export default {
     hidden: () -> @space.hide_header # FIXME this should use cascading
   methods:
     kgl: (d) -> kgl.parse d, @space
+  components:
+    sections: Sections
+
 }
 </script>
 
@@ -57,67 +36,15 @@ header {
   padding: var(--margin);
   box-sizing: border-box;
 }
-header .label {
-  font-size: 30px;
-  font-weight: 300;
-  color: black;
-}
-header .subtitle {
-  margin-top: 20px;
-  font-size: 20px;
-  font-weight: 300;
-  color: #333;
-}
-header .description {
-  margin-top: 20px;
-  font-size: 16px;
-  font-weight: 300;
-  line-height: 1.2em;
-  color: black;
-}
-
+</style>
+<style>
 /* overlay style */
 header.overlay {
   background: rgba(0,0,0,0.6);
 }
-header.overlay .label,
-header.overlay .subtitle,
-header.overlay .description {
+header.overlay .titlesection,
+header.overlay .subtitlesection,
+header.overlay .descriptionsection {
   color: white;
-}
-
-/* manuscript sender recipient */
-header .mailing {
-  margin-top: 30px;
-}
-header .mailing .sender, header .mailing .recipient {
-  display: flex;
-  color: #333;
-  font-weight: 300;
-}
-header .mailing .name {
-  font-size: 20px;
-  font-weight: 300;
-}
-header .mailing .img {
-  font-size: 60px;
-  color: rgba(0,0,0,0.54);
-  margin-right: 25px;
-}
-header .mailing .mail {
-  display: flex;
-  align-items: center;
-  margin: 0px 0px 10px 15px;
-  font-size: 22px;
-  color: rgba(0,0,0,0.3);
-}
-header .mailing .mail .arrow {
-  margin-top: -8px;
-  font-size: 30px;
-  -webkit-transform: rotate(180deg);
-  -moz-transform: rotate(180deg);
-  -ms-transform: rotate(180deg);
-  -o-transform: rotate(180deg);
-  filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
 }
 </style>
--- src/infoview_sections/Sections.vue
+++ src/infoview_sections/Sections.vue
@@ -1,5 +1,9 @@
 <script lang="coffee">
 import config from '../config.coffee'
+import TitleSection from './TitleSection.vue'
+import SubtitleSection from './SubtitleSection.vue'
+import DescriptionSection from './DescriptionSection.vue'
+import MailingSection from './MailingSection.vue'
 import TextSection from '../infobox_sections/TextSection.vue'
 import ListSection from '../infobox_sections/ListSection.vue'
 import SourceSection from '../infobox_sections/SourceSection.vue'
@@ -8,16 +12,36 @@ import RelatedSection from '../infobox_sections/RelatedSection.vue'
 import TimeSection from '../infobox_sections/TimeSection.vue'
 
 export default {
+  props:
+    template_type:
+      type: String
+      required: true
+
   render: (createElement) ->
     children = @template.map (section) =>
       createElement section.t+'section', {props:{data:@space, config:section}}
       
     return createElement 'div', {class: {sections: true}}, children
+
   computed:
     space: () -> @$store.state.selection.space
-    template: () -> if config.view_templates? and @space.template? and config.view_templates[@space.template]? then config.view_templates[@space.template] else []
+    default_config: () ->
+      info: if @space.template? then [@space.template] else [] # FIXME: template should be eventually moved within the config (cson files)
+      header: ["title", "subtitle", "description"]
+    datum_config: () -> if @space.config? then @space.config else '{}'
+    config: () -> @load_config @default_config, @datum_config, undefined, undefined
+    template: () ->
+      if config.view_templates? and @config[@template_type]?
+        return @config[@template_type].map((d) -> config.view_templates[d]).reduce (a,b) -> a.concat(b)
+      else
+        return []
+  
   components:
     textsection: TextSection
+    titlesection: TitleSection
+    subtitlesection: SubtitleSection
+    descriptionsection: DescriptionSection
+    mailingsection: MailingSection
     listsection: ListSection
     sourcesection: SourceSection
     separatorsection: SeparatorSection
