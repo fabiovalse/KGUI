d8099588ce0cc47023c28d88339098d14a25a189
fabiovalse <fabiovalse@gmail.com>
2017-08-30 15:05:16 +0200

    Added circled images.

--- src/sections/InfoboxImageSection.vue
+++ src/sections/InfoboxImageSection.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="image_section" :style="{'background-image': src ? 'url('+src+')' : 'none'}" >
+  <div class="image_section">
+    <div class="image" :class="{circled: config.circled}" :style="{'background-image': src ? 'url('+src+')' : 'none'}" ></div>
+    
     <div v-if="label !== undefined" class="fake_image ancient_background" :style="{'font-family': font_family}">
       <div class="text">{{label}}</div>
     </div>
+
     <div v-if="icon !== undefined" class="fake_image">
       <div class="icon"><i :class="'icon-'+icon"></i></div>
     </div>
@@ -33,7 +36,24 @@ export default {
 .image_section {
   height: 214px;
   background-size: cover;
-  background-color: #DDD;
+  background-color: #CCC;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.image {
+  width: 100%;
+  height: 100%;
+  background-size: var(--left-panel-width);
+}
+.circled {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  background-repeat: no-repeat;
+  background-position: 50% 20%;
+  background-size: unset;
 }
 
 .fake_image {
